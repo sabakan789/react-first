243b39e5ca2d59cdd5bb7d2691b8ef7ae8937fa0
789サバ缶 <sabanomisoni789@gmail.com>
2020-10-13 16:26:50 +0900

    state

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
-import React from "react";
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
-  );
+const App = () => <Counter></Counter>;
+
+class Counter extends Component {
+  constructor(props) {
+    super(props);
+    console.log(this.state);
+    this.state = { count: 0 };
+  }
+
+  handlePulasButton = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
+
+  handleMainasButton = () => {
+    this.setState({ count: this.state.count - 1 });
+  };
+
+  render() {
+    return (
+      <React.Fragment>
+        <div>counter: {this.state.count}</div>
+        <button onClick={this.handlePulasButton}>+1</button>
+        <button onClick={this.handleMainasButton}>-1</button>
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
