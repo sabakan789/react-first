d5d8691f4bcd010736c0786f598827e765e3ee6b
789サバ缶 <sabanomisoni789@gmail.com>
2020-10-13 16:49:19 +0900

    action

---
+++ src/actions/index.js
@@ -0,0 +1,10 @@
+export const INCREMENT = 'INCREMENT'
+export const DECREMENt = 'DECREMENT'
+
+export const increment = () => ({
+  type: "INCREMENT",
+});
+
+export const decrement = () => ({
+  type: "DECREMENT",
+});
